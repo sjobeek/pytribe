3333ef1e649f99da5c640e54ffc39fafc12943d1
sjobeek <sjobeek@gmail.com>
2014-02-12 22:29:37 +0800

    Update revision to 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
  
 setup(
     name="pytribe",
-    version="0.1.0",
+    version="0.1.1",
     long_description=__doc__,
     packages=find_packages(),
     include_package_data=True,
