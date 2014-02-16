b15b07e7ea1ff447c837af77c893ca88277d5dbe
Erik Sjoberg <sjobeek@gmail.com>
2014-02-16 18:01:11 +0800

    zoom_base BARELY working - not yet zooming at right point

--- pytribe/zoom_ui.py
+++ pytribe/zoom_ui.py
@@ -9,23 +9,21 @@ from ScreenShotWX import screencapture
 
 
 
-def scale_bitmap(bitmap, width, height):
-    image = wx.ImageFromBitmap(bitmap)
-    image = image.Scale(width, height, wx.IMAGE_QUALITY_HIGH)
-    image = image.Resize(size=(400, 400), pos=(-10, -10))
-    result = wx.BitmapFromImage(image)
-    return result
 
+def current_bitmap(zoom_map):
 
-def current_bitmap():
-
-    zm = ZoomMap(center=(400, 400), size=(400, 400))
+    zm = zoom_map
     bitmap = zm.base_bitmap
+    gaze_avg = zm.base_center
     yield bitmap
     x = 1.0
     while True:
-        x *= 1.1
-        zm.zoom_base(center=(0, 0), factor=x)
+        x *= 1.05
+        gaze_data = pytribe.query_tracker()
+        if gaze_data is not None:
+            gaze_avg_dict = gaze_data['values']['frame']['avg']
+            gaze_avg = (gaze_avg_dict['x'],gaze_avg_dict['y'])
+        zm.zoom_base(center=gaze_avg, factor=x)
         bitmap = zm.zoom_bitmap #  scale_bitmap(bitmap, 400*x, 400*x)
         yield bitmap
 
@@ -51,18 +49,29 @@ class ZoomMap(object):
 
     def zoom_base(self, center=(0, 0), factor=1.0):
 
-        _x = self.base_loc[0]
-        _y = self.base_loc[1]
+
+        #width/height to crop new image to before scaling up
         _width = self.base_size[0] / factor
         _height = self.base_size[1] / factor
+        #Amount to shift new image down/right before scaling up
+        _x = center[0] - self.base_center[0]
+        _y = center[1] - self.base_center[1]
+
+        #Prevent zooming in to sub-pixel range
         if _width <1: _width = 1
         if _height <=1: _height = 1
 
+        #Prevent zooming outside of original box
+        if _x < 0: _x = 0
+        if _y < 0: _y = 0
+        if _x + _width > self.base_size[0]: _x = self.base_size[0] - _width
+        if _y + _height > self.base_size[1]: _y = self.base_size[1] - _height
+
         #Rect can be replaced by x,y,width,height tuple
-        #sub_image = self.base_image.GetSubImage((_x, _y, _width, _height))
-        #Changes in-place
-        sub_image = self.base_image
-        sub_image.Resize(size=(_width, _height), pos=center)
+        sub_image = self.base_image.GetSubImage((_x, _y, _width, _height))
+        #Changes image in-place to
+        #sub_image = self.base_image
+        #sub_image.Resize(size=(_width, _height), pos=(-2, -2))
 
         zoomed_image = sub_image.Scale(self.base_size[0],
                                        self.base_size[1],
@@ -77,15 +86,14 @@ class ZoomCanvas(BufferedCanvas):
 
     def __init__(self, *args, **kwargs):
         #Initialization of bitmap_gen MUST come first
-        self.bitmap_gen = current_bitmap()
+        self.bitmap_gen = current_bitmap(kwargs.pop('zoom_map'))
         BufferedCanvas.__init__(self, *args, **kwargs)
 
     def draw(self, dc):
         dc.DrawBitmap(next(self.bitmap_gen), 0, 0)
 
-    def reset(self):
-        del(self.bitmap_gen)
-        self.bitmap_gen = current_bitmap()
+    def reset(self, zoom_map):
+        self.bitmap_gen = current_bitmap(zoom_map)
         self.update()
 
 
@@ -93,7 +101,7 @@ class ZoomFrame(wx.Frame):
 
     """Main Frame holding the Panel."""
     def __init__(self, *args,  **kwargs):
-        self.tick_ms = kwargs.pop('tick_ms', 100) # Remove non-standard kwargs
+        self.tick_ms = kwargs.pop('tick_ms', 25) # Remove non-standard kwargs
         wx.Frame.__init__(self, *args, **kwargs)
 
         self.raw_data_q = Queue.Queue()
@@ -108,19 +116,29 @@ class ZoomFrame(wx.Frame):
         self.timer = wx.Timer(self)
         self.Bind(wx.EVT_TIMER, self.update_zoom, self.timer)
         self.canvas = ZoomCanvas(self, wx.ID_ANY,
-                                 size=(400,400))
+                                 size=(400,400),
+                                 zoom_map=ZoomMap(center=(400,400),
+                                                  size=(400,400)))
+
+
 
         #self.timer.Start(self.tick_ms)
 
 
     def on_key_down_event(self, event):
         if event.Key == 'Numlock' and not self.key_now_down:  # \ key = Oem_5
-            print 'Ascii:', event.Key
-            self.key_now_down = True
-            self.canvas.reset() # = ZoomCanvas(self, wx.ID_ANY,
-                        #         size=(400,400))
-            self.timer.Start(self.tick_ms)
-            self.Show()
+
+            gaze_data = pytribe.query_tracker()
+            if gaze_data is not None:
+                gaze_avg_dict = gaze_data['values']['frame']['avg']
+                gaze_avg = (gaze_avg_dict['x'],gaze_avg_dict['y'])
+                zm = ZoomMap(center=gaze_avg, size=(400, 400))
+                self.SetSize(zm.base_size)
+                self.SetPosition(zm.base_loc)
+                self.key_now_down = True
+                self.canvas.reset(zm)
+                self.timer.Start(self.tick_ms)
+                self.Show()
 
         return True  # for pass through key events, False to eat Keys
 
@@ -130,7 +148,6 @@ class ZoomFrame(wx.Frame):
     def on_key_up_event(self, event):
 
         self.key_now_down = False
-        print 'Key up: ', event.Key
         self.Hide()
         self.timer.Stop()
         return True
@@ -151,10 +168,8 @@ def main():
     frame = ZoomFrame(None,
                       wx.ID_ANY,
                       title="Zoom Frame",
-                      pos=(400,400),
-                      size=(400,400),
                       style=wx.BORDER_NONE | wx.STAY_ON_TOP | wx.FRAME_NO_TASKBAR,
-                      tick_ms=50)
+                      tick_ms=25)
 
 
     hm = pyHook.HookManager()
