af153538d09b961a8701ccbea16c98a6c6003d88
Erik Sjoberg <sjobeek@gmail.com>
2014-02-22 22:25:21 +0800

    Zoom in corners now working: zoom code still needs refactoring

--- pytribe/zoom_ui.py
+++ pytribe/zoom_ui.py
@@ -9,21 +9,27 @@ from ScreenShotWX import screencapture
 import win32con
 import win32api
 
-
-def current_tracking_center():
-    pass
-
-
-def get_tracking_data():
-    pass
+# Constants:
+single_monitor_only = True
 
 
-def calc_next_zoom_center():
-    pass
+def trim_coordinate(tuple, rectangle=None):
+    """Trim a tuple to stay on the screen, or within a provided rectangle
 
-
-def log_zm():
-    pass
+    Rectangles are defined (x, y, w, h)"""
+    if single_monitor_only and rectangle is None:
+        display_x, display_y = wx.GetDisplaySize()
+        trimmed_center_x = max(0, min(tuple[0], display_x))
+        trimmed_center_y = max(0, min(tuple[1], display_y))
+    elif rectangle is not None:
+        trimmed_center_x = max(rectangle[0],
+                               min(tuple[0], rectangle[0] + rectangle[2]))
+        trimmed_center_y = max(rectangle[1],
+                               min(tuple[1], rectangle[1] + rectangle[3]))
+    else:
+        raise NotImplementedError
+        #TODO: Implement trimming for two-monitor setup
+    return trimmed_center_x, trimmed_center_y
 
 
 def parse_center(data_queue, default=None):
@@ -36,6 +42,12 @@ def parse_center(data_queue, default=None):
     return center
 
 
+def center_of_rect(rectangle):
+    center_x = int(round((rectangle[0] + rectangle[2]/2.0)))
+    center_y = int(round((rectangle[1] + rectangle[3]/2.0)))
+    return center_x, center_y
+
+
 class ZoomMap(object):
     """Contains original bitmap, plus transformed zoom coordinates.
 
@@ -43,16 +55,18 @@ class ZoomMap(object):
     def __init__(self, center=(400, 400), size=(400, 400), data_queue=None,
                  mod_key=None, zoom_factor=1.05):
 
-        self.init_center = center
-        self.current_target_center = center  # Inferred point to click
-        self.current_zoom_center = center    # Center of current zoom window
+        self.init_center = trim_coordinate(center)
         self.base_rectangle = self.calc_base_rectangle(size)
+        self.base_center = center_of_rect(self.base_rectangle)
+        self.base_loc = (self.base_rectangle[0], self.base_rectangle[1])
+        self.base_size = (self.base_rectangle[2], self.base_rectangle[3])
+
+        self.latest_gaze_center = center               # Latest reported gaze center
+        self.current_target_center = self.init_center  # Inferred point to click
+        #May confuse user if zoom_center doesn't start at base_center...  hmmm
+        self.current_zoom_center = self.init_center    # Center of current zoom
 
 
-        self.base_center = center
-        self.base_size = size
-        self.base_width = size[0]*1.0
-        self.base_height = size[1]*1.0
         self.data_q = data_queue
         self.zoom_factor = zoom_factor
         self.mod_key = mod_key
@@ -61,103 +75,109 @@ class ZoomMap(object):
         self.zoom_center_offset_x = 0.0
         self.zoom_center_offset_y = 0.0
 
-
-        self.base_loc = (int(center[0] - size[0]/2.0),
-                         int(center[1] - size[1]/2.0))
         self.previous_center = center
 
-
         #TODO: Handle case where base_loc +size includes areas outside of the screen?
-        if self.base_loc[0] < 0:
-            self.base_loc = (0, self.base_loc[1])
-        if self.base_loc[1] < 0:
-            self.base_loc = (self.base_loc[0], 0)
 
         self.base_bitmap = screencapture(self.base_loc, self.base_size)  # , debug=True)
         self.zoom_bitmap = self.base_bitmap
         self.base_image = wx.ImageFromBitmap(self.base_bitmap)
 
-    def update_zoom_center(self):
-        pass
-
     def update_target_center(self):
-        pass
+        x_int = int(round( self.current_zoom_center[0]))
+        y_int = int(round(self.current_zoom_center[1]))
+        self.current_target_center = (x_int, y_int)
 
     def calc_base_rectangle(self, full_size=(400,400)):
         """Return a rectangle (x, y, width, height) for the base bitmap"""
-        base_loc_x = max((0, int(self.init_center[0] - full_size[0]/2.0)))
-        base_loc_y = max((0, int(self.init_center[1] - full_size[1]/2.0)))
         display_x, display_y = wx.GetDisplaySize()
-        base_width  = min(display_x, base_loc_x + full_size[0])
-        base_height = min(display_y, base_loc_y + full_size[1])
-        return (base_loc_x, base_loc_y, base_width, base_height)
+        max_loc_x = min(display_x - full_size[0],
+                         int(self.init_center[0] - full_size[0]/2.0))
+        max_loc_y = min(display_y - full_size[1],
+                         int(self.init_center[1] - full_size[1]/2.0))
+        base_loc_x = max(0, max_loc_x) #int(self.init_center[0] - full_size[0]/2.0))
+        base_loc_y = max(0, max_loc_y) #int(self.init_center[1] - full_size[1]/2.0))
+
+        base_width = full_size[0]  # base_max_x - base_loc_x
+        base_height = full_size[1]  # base_max_y - base_loc_y
+        return base_loc_x, base_loc_y, base_width, base_height
 
     def zoom_size_px(self):
-        return (self.base_width * self.current_zoom_factor,
-                self.base_height * self.current_zoom_factor)
+        return (self.base_size[0] * self.current_zoom_factor,
+                self.base_size[1] * self.current_zoom_factor)
+
+    def update_zoom_center(self):
+        prev_zoom_center = self.current_zoom_center
+        zoom_pan_speed = 0.2
+        #Alternative update strategies..  hmm
+        #Display zoom center and adjust zoom center relative to prev. zoom center?
+        zoom_center_offset_x = (zoom_pan_speed *
+                                 (self.latest_gaze_center[0] - self.base_center[0]) /
+                                 self.current_zoom_factor)
+        zoom_center_offset_y = (zoom_pan_speed *
+                                 (self.latest_gaze_center[1] - self.base_center[1]) /
+                                 self.current_zoom_factor)
+        trimmed_result = trim_coordinate((prev_zoom_center[0] + zoom_center_offset_x,
+                                          prev_zoom_center[1] + zoom_center_offset_y),
+                                         self.base_rectangle)
+        self.current_zoom_center = trimmed_result
 
     def rel_zoom_loc(self):
+        """Zoom center relative to upper-left corner of zoom box"""
+        #TODO: This code seems a bit opaque and possibly wrong?  Simplify...
         zsp = self.zoom_size_px()
-        return (self.zoom_center_offset_x * self.current_zoom_factor +
-                (zsp[0] - self.base_width)/2.0,
-                self.zoom_center_offset_y * self.current_zoom_factor +
-                (zsp[1] - self.base_width)/2.0)
+        zoom_center_offset_x = self.current_zoom_center[0] - self.base_center[0]
+        zoom_center_offset_y = self.current_zoom_center[1] - self.base_center[1]
+        return (zoom_center_offset_x * self.current_zoom_factor +
+                (zsp[0] - self.base_size[0])/2.0,
+                zoom_center_offset_y * self.current_zoom_factor +
+                (zsp[1] - self.base_size[0])/2.0)
 
     def abs_zoom_loc(self):
-        return (int(self.base_center[0] + self.zoom_center_offset_x),
-                int(self.base_center[1] + self.zoom_center_offset_y))
+        return (int(self.init_center[0] + self.zoom_center_offset_x),
+                int(self.init_center[1] + self.zoom_center_offset_y))
 
-    def zoom_in(self, zoom_factor=1.05, zoomed_coords=True):
-        #TODO: Clean and refactor this entire method...  it's a mess
+    def update_gaze_center(self):
         gaze_data_update = parse_center(self.data_q)
-        if gaze_data_update not in [None, (0, 0)]:
-            self.previous_center = gaze_data_update
+        if gaze_data_update not in [None, (0.0, 0.0)]:
+            self.latest_gaze_center = gaze_data_update
 
-        center = self.previous_center
+    def update_zoom_bmp(self, zoom_factor=1.05, zoomed_coords=True):
 
         if self.current_zoom_factor <= 10:  # Don't zoom in beyond all reason...
             self.current_zoom_factor *= zoom_factor
 
-        zoom_pan_speed = 0.2
+        # Update order: gaze -> zoom -> target
+        self.update_gaze_center()
+        self.update_zoom_center()
+        self.update_target_center()
 
-        self.zoom_center_offset_x += (zoom_pan_speed *
-                                      (center[0] - self.base_center[0]) /
-                                      self.current_zoom_factor)
-        self.zoom_center_offset_y += (zoom_pan_speed *
-                                      (center[1] - self.base_center[1]) /
-                                      self.current_zoom_factor)
-
+        #TODO: Refactor zoom_in, abs_zoom_loc, and update_zoom_center
         #width/height to scale new image to
-        _width, _height = self.zoom_size_px()
-
+        scaled_width, scaled_height = self.zoom_size_px()
         #Amount to shift new image down/right before scaling up
         _x_offset, _y_offset = self.rel_zoom_loc()
-
         #Prevent zooming outside of original box
         if _x_offset < 0:  _x_offset = 0
         if _y_offset < 0:  _y_offset = 0
-        if _x_offset + self.base_size[0] > _width:
-            _x_offset = _width - self.base_size[0]
-        if _y_offset + self.base_size[1] > _height:
-            _y_offset = _height - self.base_size[1]
+        if _x_offset + self.base_size[0] > scaled_width:
+            _x_offset = scaled_width - self.base_size[0]
+        if _y_offset + self.base_size[1] > scaled_height:
+            _y_offset = scaled_height - self.base_size[1]
 
         #TODO: Trim or otherwise reduce computation for high-zoom levels...
-        zoomed_image = self.base_image.Scale(_width, _height,
+        zoomed_image = self.base_image.Scale(scaled_width, scaled_height,
                                              wx.IMAGE_QUALITY_NORMAL)
         sub_image = zoomed_image.GetSubImage((_x_offset, _y_offset,
                                               self.base_size[0], self.base_size[1]))
 
         self.zoom_bitmap = wx.BitmapFromImage(sub_image)
-
-    def next_zoom_bitmap(self):
-        """Returns the next bitmap to be displayed on screen"""
-        self.zoom_in(zoom_factor=self.zoom_factor)
         return self.zoom_bitmap
 
-    def click_zoom_target(self):
+    def click_target_center(self):
         """Clicks once on zoom target"""
         prev_x, prev_y = win32api.GetCursorPos()
-        click_x, click_y = self.abs_zoom_loc()
+        click_x, click_y = self.current_target_center
         print "Final Click Location: ", (click_x, click_y)
         win32api.SetCursorPos((click_x, click_y))
         if self.mod_key == 'ChangeMeToEnable':  # Set mod-key for right-click if desired
@@ -256,7 +276,7 @@ class ZoomFrame(wx.Frame):
 
 
     def click_target(self):
-        self.current_zoom_map.click_zoom_target()
+        self.current_zoom_map.click_target_center()
         pass
 
 
@@ -298,7 +318,7 @@ class ZoomCanvas(BufferedCanvas):
         BufferedCanvas.__init__(self, *args, **kwargs)
 
     def draw(self, dc):
-        dc.DrawBitmap(self.zm.next_zoom_bitmap(), 0, 0)
+        dc.DrawBitmap(self.zm.update_zoom_bmp(), 0, 0)
 
     def reset(self, zoom_map):
         self.zm = zoom_map
